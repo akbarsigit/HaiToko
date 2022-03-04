89a557f4c9c25b615f581dab8c1aae7f33d7f5ed
akbarsigit <akbarsigit3@gmail.com>
2022-03-05 01:21:08 +0700

    First Commit

---
+++ HaiToko/HaiTokoWeb/HaiTokoBackEnd/src/main/java/com/haitoko/admin/HaiTokoBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.haitoko.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaiTokoBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaiTokoBackEndApplication.class, args);
+	}
+
+}
---
+++ HaiToko/HaiTokoWeb/HaiTokoFrontEnd/src/main/java/com/haitoko/site/HaiTokoFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.haitoko.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaiTokoFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaiTokoFrontEndApplication.class, args);
+	}
+
+}
